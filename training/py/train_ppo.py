c3bd1756958f81a759ef45358db79119331954a1
chris111123max <3220251075@bit.edu.cn>
2026-09-10 16:51:42 +0800

    sssss

--- training/py/train_ppo.py
+++ training/py/train_ppo.py
@@ -16,6 +16,7 @@ if str(PYTHON_ROOT) not in sys.path:
     sys.path.insert(0, str(PYTHON_ROOT))
 
 import numpy as np
+import torch as th
 from stable_baselines3.common.callbacks import CallbackList
 from stable_baselines3.common.utils import get_schedule_fn
 
